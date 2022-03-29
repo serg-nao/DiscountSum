080996d2d0b967104ff27eb518a68223b7b24aa7
serg-nao <serg-nao@rambler.ru>
2022-03-29 22:28:49 +0300

    Code changed

--- src/main/kotlin/DiscountSum.kt
+++ src/main/kotlin/DiscountSum.kt
@@ -2,14 +2,14 @@ package ru.netology
 
 fun main() {
     val lastAmount: Int = 15400
-    val thisAmount: Int = 7000
+    val currentAmount: Int = 7000
     val privelege: Boolean = true
     val maxDiscount: Int = 5
 
     val discount: Int = if (lastAmount >= 10001) maxDiscount * lastAmount / 100 else if (lastAmount >= 1001) 100 else 0
-    val privelegeDiscount: Double = if (privelege) 0.99 else 1.00
-    val finalSum: Int = ((thisAmount - discount) * privelegeDiscount).toInt()
-    val totalDiscount: Int = thisAmount - finalSum
+    val ratio: Double = if (privelege) 0.99 else 1.00
+    val finalSum: Int = ((currentAmount - discount) * ratio).toInt()
+    val totalDiscount: Int = currentAmount - finalSum
 
     println("Сумма платежа: $finalSum руб. Общая сумма скидки $totalDiscount руб.")
 
